ab0bb4c8daecba48393c36f20f4f3d6c9c58a8ce
David <davidlaugon@gmail.com>
2019-11-07 19:11:50 -0600

    Tarea 7

--- TAREAS/7/main.c
+++ TAREAS/7/main.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
-#include <math.h>
-int main(int argc, char *argv[])
-{
-	int x;
-	int y;
-	int z;
-	x = atoi(argv[1]);
-	y = atoi(argv[2]);
-	z = pow(x, y);
-	printf("%i", z);
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+	
+	long int x, y, a, b; 
+	x = atoi(argv[1]); 
+	y = atoi(argv[2]); 
+	a = 1; 
+	b = 1; 
+	
+	do{ 
+		b=b*x; 
+		a++; 
+	}while(a<=y); 
+	printf("%li\n", b);  
 	
 	return 0;
 }
